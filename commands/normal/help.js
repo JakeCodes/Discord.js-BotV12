d07c7e8215a9941de7be65c166aa9da8752813fe
EpixCodes <jakethesnake20006@gmail.com>
2020-08-09 02:52:15 -0500

    Updated

--- commands/normal/help.js
+++ commands/normal/help.js
@@ -7,10 +7,17 @@ module.exports = {
         var embed = new Discord.MessageEmbed()
             .setTitle("Help")
             .setDescription("List of commands for me to follow")
+
             .addField('Website (No Arg)', `Returns my website`)
-            .addField(`8ball (Question Arg)`, `Returns yes or no statement`)
+            .addField(`8ball (Question)`, `Returns yes or no statement`)
             .addField(`Ping (No Arg)`, `Returns Pong`)
             .addField(`Rules (No Arg)`, `Returns List of rules`)
+            .addField(`Join (No Arg)`, `Joins VC`)
+            .addField(`Leave (No Arg)`, `Leaves VC`)
+            .addField(`Pause (No Arg)`, `Pauses Video`)
+            .addField(`Resume (No Arg)`, `Resumes Video`)
+            .addField(`Play (YT Link)`, `Plays YT Link Audio`)
+
             .setColor(15158332)
 
         message.channel.send({embed: embed});
