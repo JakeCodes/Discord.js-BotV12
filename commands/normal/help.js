b11c1bdeda686b5d799019c9e35eb6022c195646
EpixCodes <jakethesnake20006@gmail.com>
2020-08-09 20:48:51 -0500

    udpated

--- commands/normal/help.js
+++ commands/normal/help.js
@@ -4,7 +4,23 @@ module.exports = {
     name: "help",
     description: "help command",
     run: (message, args) => {
-        var embed = new Discord.MessageEmbed()
+
+        if (!args[0]) {
+            var embed = new Discord.MessageEmbed()
+                .setTitle("Help")
+                .setDescription("List of commands for me to follow")
+
+                .addFields(
+                    {name: 'Help All', value: `Returns All Commands`},
+                    {name: 'Help Music', value: `Returns Music Commands`},
+                    {name: 'Help Misc',    value: `Returns Misc Commnands`},
+                )
+
+                .setColor(15158332)
+        }
+
+        else if (args[0] === "all") {
+            var embed = new Discord.MessageEmbed()
             .setTitle("Help")
             .setDescription("List of commands for me to follow")
 
@@ -19,8 +35,33 @@ module.exports = {
                 {name: 'Resume (No Arg)',  value: `Resumes Video`},
                 {name: 'Play (YT Link)',   value: `Plays YT Link Audio`}
             )
+        }
 
-            .setColor(15158332)
+        else if (args[0] === "music") {
+            var embed = new Discord.MessageEmbed()
+            .setTitle("Help Music")
+            .setDescription("List of commands for me to follow")
+
+            .addFields(
+                {name: 'Join (No Arg)',    value: `Joins VC`},
+                {name: 'Leave (No Arg)',   value: `Leaves VC`},
+                {name: 'Pause (No Arg)',   value: `Pauses Video`},
+                {name: 'Resume (No Arg)',  value: `Resumes Video`},
+                {name: 'Play (YT Link)',   value: `Plays YT Link Audio`}
+            )
+        }
+
+        else if (args[0] === "misc") {
+            var embed = new Discord.MessageEmbed()
+            .setTitle("Help")
+            .setDescription("List of commands for me to follow")
+
+            .addFields(
+                {name: 'Rules (No Arg)', value: `Returns List of Rules`},
+                {name: 'Website (Question)', value: `Returns My Website`},
+                {name: 'Ping (No Arg)', value: `Returns Pong`}
+            )
+        }
 
         message.channel.send({embed: embed});
     }
