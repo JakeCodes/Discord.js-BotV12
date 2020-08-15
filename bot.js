541706738faa3870881993ee51819a47f174ecf3
EpixCodes <jakethesnake20006@gmail.com>
2020-08-15 02:20:09 -0500

    Cleaned Code

--- bot.js
+++ bot.js
@@ -10,24 +10,27 @@ client.events = new Collection();
 const token = process.env.TOKEN
 const fs = require('fs')
 
-const adminCMDFiles = fs.readdirSync("./commands/mod").filter(file => file.endsWith('.js'));
 
+// Mod Commands
+const adminCMDFiles = fs.readdirSync("./commands/mod").filter(file => file.endsWith('.js'));
 adminCMDFiles.forEach(file => {
     const adminCommand = require(`./commands/mod/${file}`);
 
     client.commands.set(adminCommand.name, adminCommand)
 })
 
-const normalCMDFiles = fs.readdirSync("./commands/normal").filter(file => file.endsWith('.js'));
 
+// Normals Commands
+const normalCMDFiles = fs.readdirSync("./commands/normal").filter(file => file.endsWith('.js'));
 normalCMDFiles.forEach(file => {
     const normalCommand = require(`./commands/normal/${file}`);
 
     client.commands.set(normalCommand.name, normalCommand)
 })
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
+// Events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 eventFiles.forEach(file => {
     const events = require(`./events/${file}`);
     const event = file.split('.')[0];
