eb0433de7be1df1f518d942037791d209dbff76c
EpixCodes <jakethesnake20006@gmail.com>
2020-08-15 18:30:46 -0500

    updated ready event

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
-const prefix = process.env.PREFIX.trim().toLowerCase();
+const prefix = process.env.PREFIX.toLowerCase();
 
 module.exports = (client, message) => {
     // set status and log when ready
-    client.user.setActivity( `${prefix} help`, {
+    client.user.setActivity( `${prefix}help`, {
         type: "LISTENING",
       });
     console.log("Bot is ready")
