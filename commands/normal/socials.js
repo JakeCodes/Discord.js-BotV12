1c13c09b7502d592ba6ffbd8ead7e36e8aa17114
EpixCodes <jakethesnake20006@gmail.com>
2020-08-15 00:40:13 -0500

    Cleaned Code

--- commands/normal/socials.js
+++ commands/normal/socials.js
@@ -4,19 +4,19 @@ module.exports = {
     name: "socials",
     description: "returns socials",
     run: (message, args) => {
-            var embed = new MessageEmbed()
-                .setTitle("Socials")
-                .setDescription("All of my socials")
+        var embed = new MessageEmbed()
+            .setTitle("Socials")
+            .setDescription("All of my socials")
 
-                .addFields(
-                    {name: 'Website', value: `https://jakecodes.com`, inline: false },
-                    {name: 'Steam', value: `https://steamcommunity.com/id/jake-codes`, inline: false },
-                    {name: 'Stackoverflow', value: `https://stackoverflow.com/users/13073672/epix?tab=profile`, inline: false },
-                    {name: 'Github', value: `https://github.com/JakeCodes`, inline: false },
-                )
+            .addFields(
+                {name: 'Website', value: `https://jakecodes.com`, inline: false },
+                {name: 'Steam', value: `https://steamcommunity.com/id/jake-codes`, inline: false },
+                {name: 'Stackoverflow', value: `https://stackoverflow.com/users/13073672/epix?tab=profile`, inline: false },
+                {name: 'Github', value: `https://github.com/JakeCodes`, inline: false },
+            )
 
-                .setColor(15158332)
+            .setColor(15158332)
 
-            message.channel.send({embed: embed})
+        message.channel.send({embed: embed})
     }
 }
