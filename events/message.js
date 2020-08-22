5dda955652ac642d4c04995537338777cd14d3d2
EpixCodes <jakethesnake20006@gmail.com>
2020-08-22 16:10:08 -0500

    commands are not case sensitive

--- events/message.js
+++ events/message.js
@@ -2,6 +2,8 @@ const prefix = process.env.PREFIX.toLowerCase();
 
 module.exports = (client, message) => {
     if (message.author.bot) return;
+
+    message.content = message.content.toLowerCase()
   
     if (message.content.indexOf(prefix) !== 0) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
