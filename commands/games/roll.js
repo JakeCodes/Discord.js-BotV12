03ad7a3e652cbb9c512f7c9c464b473055b68b19
EpixCodes <jakethesnake20006@gmail.com>
2020-08-22 23:23:52 -0500

    fixed roll command

--- commands/games/roll.js
+++ commands/games/roll.js
@@ -7,7 +7,7 @@ module.exports = {
     run: (message, args) => {
         var embed = new MessageEmbed()
             .setTitle("Dice Roll")
-            .setDescription(`${Math.floor(Math.random() * 7) + 1}`)
+            .setDescription(`${Math.floor(Math.random() * 6) + 1}`)
             .setColor(15158332)
 
         message.channel.send({embed: embed})
