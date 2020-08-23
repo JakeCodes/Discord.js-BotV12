cb9de7c04aeb59ed48c340368de44d23ee1dc3d1
EpixCodes <jakethesnake20006@gmail.com>
2020-08-22 23:15:24 -0500

    updated

--- commands/games/random.js
+++ commands/games/random.js
@@ -8,7 +8,7 @@ module.exports = {
         if (!args[0] || !args[1]) return message.reply("You must have at least 2 args")
 
         const random = Math.floor(Math.random() * args.length)
-        const randomStr = ar[random]
+        const randomStr = args[random]
         console.log(randomStr)
 
         var embed = new MessageEmbed()
