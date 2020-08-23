1bcc2ed23ec330e8c6b43baa2fddee947f18c441
EpixCodes <jakethesnake20006@gmail.com>
2020-08-22 23:04:16 -0500

    simplified code

--- commands/games/random.js
+++ commands/games/random.js
@@ -5,19 +5,14 @@ module.exports = {
     description: "returns random string",
     aliases: ["diceroll", "rolldice"],
     run: (message, args) => {
-        
         if (!args[0] || !args[1]) return message.reply("You must have at least 2 args")
-        const ar = []
-        args.forEach(arg => {
-            ar.push(arg);
-        })
 
-        const random = Math.floor(Math.random() * ar.length)
+        const random = Math.floor(Math.random() * args.length)
         const randomStr = ar[random]
         console.log(randomStr)
 
         var embed = new MessageEmbed()
-            .setTitle("Random Str")
+            .setTitle("Random")
             .setDescription(`${randomStr}`)
             .setColor(15158332)
 
