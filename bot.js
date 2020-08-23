737ea6858754ff5638c8a553be7ebf62f756db2e
EpixCodes <jakethesnake20006@gmail.com>
2020-08-23 10:04:17 -0500

    updated

--- bot.js
+++ bot.js
@@ -3,22 +3,16 @@ const { Collection, Client } = require("discord.js")
 const { registerCommands, registerEvents } = require('./utils/registry');
 const { Player } = require("discord-player")
 
-// From Discord
 const client = new Client()
 client.commands = new Collection();
 client.events = new Collection();
 
-// Player
 const player = new Player(client)
 client.player = player;
 
-// Other
-const token = process.env.TOKEN
 const fs = require('fs')
 
-// Register Events & Commands
 registerEvents(client, '../events');
 registerCommands(client, '../commands');
 
-// Login
-client.login(token)
+client.login(process.env.TOKEN)
