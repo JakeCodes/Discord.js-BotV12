d2cb66d6aac00527bd1597735fec33edd61bc4ad
EpixCodes <jakethesnake20006@gmail.com>
2020-10-01 10:57:47 -0500

    updated

--- commands/info/socials.js
+++ commands/info/socials.js
@@ -1,26 +1,26 @@
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 
-const website = "https://jakecodes.com"
-const steam = "https://steamcommunity.com/id/jake-codes"
-const stackoverflow = "https://stackoverflow.com/users/13073672/epix?tab=profile"
-const github = "https://github.com/JakeCodes"
-
+const website = "https://vue.jakecodes.com";
+const steam = "https://steamcommunity.com/id/jake-codes";
+const stackoverflow =
+  "https://stackoverflow.com/users/13073672/epix?tab=profile";
+const github = "https://github.com/JakeCodes";
 
 module.exports = {
-    name: "socials",
-    description: "returns socials",
-    aliases: [],
-    run: (message, args) => {
-        var embed = new MessageEmbed()
-            .addFields(
-                {name: 'Website', value: `[Here](${website})`},
-                {name: 'Steam', value: `[Here](${steam})`},
-                {name: 'Stackoverflow', value: `[Here](${stackoverflow})`},
-                {name: 'Github', value: `[Here](${github})`},
-            )
+  name: "socials",
+  description: "returns socials",
+  aliases: [],
+  run: (message, args) => {
+    var embed = new MessageEmbed()
+      .addFields(
+        { name: "Website", value: `[Here](${website})` },
+        { name: "Steam", value: `[Here](${steam})` },
+        { name: "Stackoverflow", value: `[Here](${stackoverflow})` },
+        { name: "Github", value: `[Here](${github})` }
+      )
 
-            .setColor(15158332)
+      .setColor(15158332);
 
-        message.channel.send(embed)
-    }
-}
+    message.channel.send(embed);
+  },
+};
