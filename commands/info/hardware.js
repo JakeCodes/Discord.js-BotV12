ce2c36bc324b140114a451b540e122f205c49423
JakeCodes <jakethesnake20006@gmail.com>
2021-10-16 18:14:14 -0500

    updated hardware

--- commands/info/hardware.js
+++ commands/info/hardware.js
@@ -7,7 +7,7 @@ const graphicsCard = "https://www.amazon.com/ASUS-Geforce-Phoenix-Graphics-PH-GT
 const ssd = "https://www.amazon.com/Blue-NAND-1TB-SSD-WDS100T2B0A/dp/B073SBQMCX/ref=sr_1_4?dchild=1&keywords=931GB+Western+Digital+WDC+WDS100T2B0A-00SM50&qid=1597792700&sr=8-4"
 const hdd = "https://www.amazon.com/Toshiba-5400RPM-Cache-Notebook-MQ01ABD100V/dp/B07NGTZWWZ/ref=sr_1_2?crid=XXOA1W3XFLNC&dchild=1&keywords=1tb+hdd+toshiba&qid=1597792818&sprefix=1tb+hdd+Tos%2Caps%2C185&sr=8-2"
 const headset = "https://www.amazon.com/dp/B0748K7JW6/ref=dp_cerb_2"
-const mouse = "https://www.amazon.com/Rat%C3%B3n-rendimiento-jugar-videojuegos-Logitech/dp/B07GBZ4Q68/ref=sr_1_1?dchild=1&keywords=Logitech+G502+Hero&qid=1597792965&sr=8-1"
+const mouse = "https://www.amazon.com/Logitech-Wireless-Gaming-Esports-Performance/dp/B07GCKQD77/ref=asc_df_B07GCKQD77/?tag=&linkCode=df0&hvadid=309751315916&hvpos=&hvnetw=g&hvrand=4539051909659616698&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9027821&hvtargid=pla-523894557330&ref=&adgrpid=67183599252&th=1"
 const keyboard = "https://www.amazon.com/Logitech-Spectrum-Mechanical-Keyboard-920-008012/dp/B01GAONOAC/ref=sr_1_1?dchild=1&keywords=Logitech+G910+Orion+Spectrum&qid=1597792978&sr=8-1"
 
 
@@ -25,7 +25,7 @@ module.exports = {
                 {name: 'Storage', value: `SSD: [931GB Western Digital WDC WDS100T2B0A-00SM50](${ssd})
                                           HDD: [931GB TOSHIBA HDWD110](${hdd})`},
                 {name: 'Headset', value: `[Corsair VOID PRO Wireless](${headset})`},
-                {name: 'Mouse', value: `[Logitech G502 Hero](${mouse})`},
+                {name: 'Mouse', value: `[Logitech G Pro Wireless](${mouse})`},
                 {name: 'Keyboard', value: `[Logitech G910 Orion Spectrum](${keyboard})`},
             )
 
